52a2eae81f6f9226e1b70bc3e317b8aa1f33ee8b
Pradosh Sinha <pradoshks@outlook.com>
2017-05-15 21:43:04 +1000

    Refactored all the logic for calculating days from any day of the month including leap year. For better reusability and separation of concerns.

--- src/js/DaysDifference.js
+++ src/js/DaysDifference.js
@@ -1,33 +1,37 @@
 export default class DaysDifference {
 
-  constructor(startDate, endDate) {
+  constructor (startDate, endDate) {
     this.startDate = startDate;
     this.endDate = endDate;
     this.totalMonths = 12;
   }
 
-  get daysInMonth() {
+  daysInMonth (year) {
     // Each index represents month.
     const daysinMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    // Check if leap year.
+    if (year % 4 == 0) {
+      daysinMonths[1] = 29;
+      return daysinMonths;
+    }
     return daysinMonths;
   }
-  getRemainingDaysFromStartMonth(day, month, year) {
-    let months = this.leapYear(year);
+
+  getRemainingDaysFromStartMonth (day, month, year) {
+    let months = this.daysInMonth(year);
     // Returns the day inclusive by adding 1
-    return (months[month-1] - day) + 1;
+    return (months[month - 1] - day) + 1;
   }
 
-  getRemainingDaysFromEndMonth(day, month, year) {
-    let months = this.leapYear(year);
+  getRemainingDaysFromEndMonth (day, month, year) {
+    let months = this.daysInMonth(year);
     // Returns the day inclusive by adding 1
     return (day - 1) + 1;
   }
 
-  adjustDaysInMonths(daysInMonthsArray) {
-    if(this.startDate.year == this.endDate.year && this.startDate.month == this.endDate.month) {
-      // Will return just one index if same month in same start and end year.
-      return [(this.endDate.day - this.startDate.day) + 1];
-    }
+  // Adjust number of days in case dates that are not the 1st of month.
+  adjustDaysInMonths (daysInMonthsArray) {
+
     let daysFromStartMonth = this.getRemainingDaysFromStartMonth(this.startDate.day, this.startDate.month, this.startDate.year);
     let daysFromEndMonth = this.getRemainingDaysFromEndMonth(this.endDate.day, this.endDate.month, this.endDate.year);
 
@@ -38,39 +42,94 @@ export default class DaysDifference {
 
   }
 
-  get daysfromSameYear() {
-    let months = this.leapYear(this.startDate.year);
+  // Adjust number of days in case dates that are not the 1st of month.
+  adjustDaysMonths (daysInMonthsArray, year, month, day, startOrEnd = 'start') {
+
+    let daysMonth = [];
+
+    if (startOrEnd === 'start') {
+      daysMonth = this.getRemainingDaysFromStartMonth(day, month, year);
+    } else {
+      daysMonth = this.getRemainingDaysFromEndMonth(day, month, year);
+    }
+
+    daysInMonthsArray[month - 1] = daysMonth;
+
+    return daysInMonthsArray;
+
+  }
+
+  // Months array with each element as no of days.
+  monthsStartDate () {
+    // In case we have middle of month date.
+    let months = this.adjustDaysMonths(this.daysInMonth(this.startDate.year), this.startDate.year, this.startDate.month, this.startDate.day, 'start');
+    // Includes any middle month, 1st or last.
+    // Slice accordingly.
+    return months.slice(this.startDate.month - 1);
+  }
+
+  // Months array with each element as no of days.
+  monthsEndDate () {
+    // In case we have middle of month date.
+    let months = this.adjustDaysMonths(this.daysInMonth(this.endDate.year), this.endDate.year, this.endDate.month, this.endDate.day, 'end');
+    // Includes any middle month, 1st or last.
+    // Slice accordingly.
+    return months.slice(0, this.endDate.month);
+  }
+
+  get daysfromSameYear () {
+    let remainingMonths;
+    if (this.startDate.year == this.endDate.year && this.startDate.month == this.endDate.month) {
+      // Will return just one index if same month in same start and end year.
+      return (this.endDate.day - this.startDate.day) + 1;
+    }
+
+    let daysInMonthsForStart = this.monthsStartDate();
+    let daysInMonthsForEnd = this.monthsEndDate();
+
+    // We already have sliced from start month.
+    remainingMonths = daysInMonthsForStart.slice(0, (this.endDate.month - this.startDate.month) + 1);
+    // Take the last month out which will be replaced with the last index of daysInMonthsForEnd.
+    remainingMonths.pop();
+
+    // the last element form daysInMonthsForEnd is the actual calculated days for the end date month.
+    remainingMonths.push(daysInMonthsForEnd[daysInMonthsForEnd.length - 1]);
+
+    console.log('remainingMonths:', remainingMonths);
+
+    return remainingMonths.reduce((prev, curr) => prev + curr);
+
+  }
+
+  get daysfromSameYearOLD () {
+    if (this.startDate.year == this.endDate.year && this.startDate.month == this.endDate.month) {
+      // Will return just one index if same month in same start and end year.
+      return (this.endDate.day - this.startDate.day) + 1;
+    }
+    let months = this.daysInMonth(this.startDate.year);
     let remainingMonths;
 
-    let daysInMonthsArrayAdjusted = this.adjustDaysInMonths(months);
+    // There could be dates from middle of month.
+    let daysInMonthsAdjustedForStart = this.adjustDaysMonths(months, this.startDate.year, this.startDate.month, this.startDate.day, 'start');
+    console.log('daysInMonthsAdjustedForStart:', daysInMonthsAdjustedForStart);
+    // Adjust days for the end date if any middle dates.
+    let daysInMonthsAdjustedForEnd = this.adjustDaysMonths(months, this.endDate.year, this.endDate.month, this.endDate.day, 'end');
 
-    if(daysInMonthsArrayAdjusted.length == 1) {
-      return daysInMonthsArrayAdjusted[0];
+    if (this.startDate.month == 1 && this.endDate.month == 12 || this.startDate.month > 1 && this.endDate.month == 12) {
+      remainingMonths = daysInMonthsAdjusted.slice(this.startDate.month - 1);
     }
 
-    if(this.startDate.month == 1 && this.endDate.month == 12 || this.startDate.month > 1 && this.endDate.month == 12) {
-      remainingMonths = months.slice(this.startDate.month - 1);
+    if (this.startDate.month > 1 && this.endDate.month < 12) {
+      remainingMonths = daysInMonthsAdjusted.slice(this.startDate.month - 1, this.endDate.month);
     }
 
-    if(this.startDate.month > 1 && this.endDate.month < 12) {
-      remainingMonths = months.slice(this.startDate.month - 1, this.endDate.month + 1);
-    }
+    console.log(remainingMonths);
 
     return remainingMonths.reduce((prev, curr) => prev + curr);
   }
 
-  leapYear(year) {
-    if (year % 4 == 0) {
-      let months = this.daysInMonth;
-      months[1] = 29;
-      return months;
-    }
-    return this.daysInMonth;
+  get daysFromStartDate () {
+    let totalMonths = (this.totalMonths - this.startDate.month) + 1;
   }
-
-  get daysFromStartDate() {
-    let totalMonths = (this.totalMonths - this.startDate.month) + 1
-  }
-
 
 }
--- src/js/DaysDifference.test.js
+++ src/js/DaysDifference.test.js
@@ -1,37 +1,81 @@
 var expect = require('chai').expect;
+var assert = require('chai').assert;
 
 var DaysDifference = require('./DaysDifference').default;
 
+describe('Test DaysDifference class', function () {
 
-describe('Testing DaysDifference class', function() {
-
-  it('should return days 365 from 1/1/2017 to 31/12/2017', function() {
+  it('should return days 365 from 1/1/2017 to 31/12/2017', function () {
     var startDate = {day: 1, month: 1, year: 2017};
     var endDate = {day: 31, month: 12, year: 2017};
     var calculateDays = new DaysDifference(startDate, endDate);
-    return expect(calculateDays.daysfromSameYear).to.equal(365)
+    return expect(calculateDays.daysfromSameYear).to.equal(365);
   });
 
-  it('should return days 366 from leap year: 1/1/2016 to 31/12/2016', function() {
+  it('should return days 366 in leap year from 1/1/2016 to 31/12/2016', function () {
     var startDate = {day: 1, month: 1, year: 2016};
     var endDate = {day: 31, month: 12, year: 2016};
     var calculateDays = new DaysDifference(startDate, endDate);
-    return expect(calculateDays.daysfromSameYear).to.equal(366)
+    return expect(calculateDays.daysfromSameYear).to.equal(366);
   });
 
-  it('should return days 292 from 15/3/2016 to 31/12/2016', function() {
+  it('should return 292 days from 15/3/2016 to 31/12/2016', function () {
     var startDate = {day: 15, month: 3, year: 2016};
     var endDate = {day: 31, month: 12, year: 2016};
     var calculateDays = new DaysDifference(startDate, endDate);
-    return expect(calculateDays.daysfromSameYear).to.equal(292)
+    return expect(calculateDays.daysfromSameYear).to.equal(292);
   });
 
+  it('should return 336 days from 15/1/2016 to 15/12/2016', function () {
+    var startDate = {day: 15, month: 1, year: 2016};
+    var endDate = {day: 15, month: 12, year: 2016};
+    var daysDiff = new DaysDifference(startDate, endDate);
+    expect(daysDiff.daysfromSameYear).to.equal(336);
+  });
 
-  it('should return days 17 from 15/3/2016 to 31/3/2016 (same start and end month from same year)', function() {
+  it('should return 366 days from 1/1/2016 to 31/12/2016', function () {
+    var startDate = {day: 1, month: 1, year: 2016};
+    var endDate = {day: 31, month: 12, year: 2016};
+    var daysDiff = new DaysDifference(startDate, endDate);
+    expect(daysDiff.daysfromSameYear).to.equal(366);
+  });
+
+  it('should return 134 days from 1/1/2017 to 31/12/2017', function () {
+    var startDate = {day: 4, month: 8, year: 2017};
+    var endDate = {day: 15, month: 12, year: 2017};
+    var daysDiff = new DaysDifference(startDate, endDate);
+    expect(daysDiff.daysfromSameYear).to.equal(134);
+  });
+
+  it('should return days 17 from 15/3/2016 to 31/3/2016 (same start and end month from same year)', function () {
     var startDate = {day: 15, month: 3, year: 2016};
     var endDate = {day: 31, month: 3, year: 2016};
     var calculateDays = new DaysDifference(startDate, endDate);
-    return expect(calculateDays.daysfromSameYear).to.equal(17)
+    return expect(calculateDays.daysfromSameYear).to.equal(17);
   });
+
+  it('should return days 81 from 15/3/2016 to 3/6/2016', function () {
+    var startDate = {day: 15, month: 3, year: 2016};
+    var endDate = {day: 3, month: 6, year: 2016};
+    var calculateDays = new DaysDifference(startDate, endDate);
+    return expect(calculateDays.daysfromSameYear).to.equal(81);
+  });
+
+  it('should return remaining months and adjusted days from start date 15/3/2016 and end date 3/6/2016', function () {
+    var startDate = {day: 15, month: 3, year: 2016};
+    var endDate = {day: 3, month: 6, year: 2016};
+    var daysDiff = new DaysDifference(startDate, endDate);
+    expect(daysDiff.monthsStartDate()).to.deep.equal([17, 30, 31, 30, 31, 31, 30, 31, 30, 31]);
+    return expect(daysDiff.monthsEndDate()).to.deep.equal([31, 29, 31, 30, 31, 3]);
+  });
+
+  it('should return remaining months and adjusted days from start date 15/1/2016 and end date 15/12/2016', function () {
+    var startDate = {day: 15, month: 1, year: 2016};
+    var endDate = {day: 15, month: 12, year: 2016};
+    var daysDiff = new DaysDifference(startDate, endDate);
+    expect(daysDiff.monthsStartDate()).to.deep.equal([17, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]);
+    expect(daysDiff.monthsEndDate()).to.deep.equal([31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 15]);
+  });
+
 
 });
